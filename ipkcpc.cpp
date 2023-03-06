5d0d862fd68d025da05917c06d148b9446edfef7
Štefan Pekník <xpekni01@stud.fit.vutbr.cz>
2023-03-06 20:42:26 +0100

    create dafult body with includes

---
+++ ipkcpc.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <signal.h>
+
+#define BUFSIZE 1024
+
+int main(int argc, char *argv[])
+{
+
+  return EXIT_SUCCESS;
+}

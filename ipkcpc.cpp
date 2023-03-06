ea79abed0086d1d5dbb9cfa63ad5ccc2906d9531
Štefan Pekník <xpekni01@stud.fit.vutbr.cz>
2023-03-06 20:43:28 +0100

    declare vars

--- ipkcpc.cpp
+++ ipkcpc.cpp
@@ -13,6 +13,16 @@
 
 #define BUFSIZE 1024
 
+int client_socket, port_number, bytestx, bytesrx;
+socklen_t serverlen;
+const char *server_hostname;
+struct hostent *server;
+struct sockaddr_in server_address;
+char buf[BUFSIZE];
+
+std::string host;
+int port = 0;
+std::string mode;
 int main(int argc, char *argv[])
 {
 
